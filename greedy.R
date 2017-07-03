3fe72d1fb84128125abf26b8664fea6532f9ad3f
ciaran <ctadm66@yahoo.ie>
2017-07-03 12:14:26 +0100

    Fixed a bug in greedy that occurred very rarely when 2 different towns would have the exact same distance to each other

--- greedy.R
+++ greedy.R
@@ -9,8 +9,16 @@ getGreedy<-function(size,d){
     if(i==size){
       v[track]<-1
     }else{
-      if(length(d[track,-exc])==1){ #it was being forced to a vector when there was length 1 and it was causing issues
-        v[track]<-which(d[track,]==d[track,-exc])
+      if(length(d[track,-exc])==1){ #it was being forced to a vector when there was length 1 and it was causing issues with "names()"
+        #cat("track: ",track,"\n")
+        #cat("d[track,-exc]",d[track,-exc],"\n")
+        #cat("exc: ",exc,"\n")
+        #cat("length: ",length(d[track,-exc])==1,"\n")
+        #cat("which: ",which(d[track,]==d[track,-exc]),"\n")
+        #v[track]<-which(d[track,]==d[track,-exc])
+        vect2<-which(d[track,]==d[track,-exc]) #PROBLEM this can produce more than 1 number when there is 2 numbers exactly the same in the same row
+        pos<-setdiff(vect2,exc) #This takes the differene between the 2 vectors i.e removes the excluded towns from vect2 (if an excluded town has exactly the same distance it can be included)
+        v[track]<-pos
       }else{
         v[track]<-as.integer(names(which.min(d[track,-exc])))
       }
@@ -41,10 +49,14 @@ valid<-function(b){ #checks validity of path, i.e inf it contains loops return f
   #print(visited)
   return(!any(duplicated(visited))) # ! used because I want it to return true is there is no duplicates
 }
-Xs<-sample(1:100,10)
-Ys<-sample(1:100,10)
+valids<-numeric(100)
+for(i in 1:1000){
+Xs<-sample(1:100,20)
+Ys<-sample(1:100,20)
 Ps<-cbind(Xs,Ys)
 d<-as.matrix(dist(Ps))
 
-vec<-getGreedy(10,d)
-valid(vec)
+vec<-getGreedy(20,d)
+valids[i]<-valid(vec)
+}
+valids
